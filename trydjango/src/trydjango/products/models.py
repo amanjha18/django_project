509bdf9eda4c59e2af3b9c81a14777da30e75201
amanjha18 <amanjha18@navgurukul.org>
2019-08-07 18:50:48 +0530

    add new feature

--- trydjango/src/trydjango/products/models.py
+++ trydjango/src/trydjango/products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title         = models.TextField()
-    description   = models.TextField()
-    price         = models.TextField()
-    summary       = models.TextField(default="this is cool! ")
-
+    title         = models.CharField(max_length=120) #max_length = required
+    description   = models.TextField(blank=True, null=True) #field is optional to all circumstances.
+    price         = models.IntegerField()
+    summary       = models.TextField(blank=False, null=False) #value is required in all circumstances.
+    featured      = models.BooleanField(default=False) #null=True, default=True
--- trydjango/src/trydjango/products/views.py
+++ trydjango/src/trydjango/products/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from.forms import ProductForm
 from .models import Product
 # Create your views here.
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+      form.save()
+      form=ProductForm()
+
+    context={
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+
 def product_detail_view(request):
     obj=Product.objects.get(id=1)
+    #'''context={
+    #    'title': obj.title,
+     #   'description': obj.description
+   # }'''
     context={
-        'title': obj.title,
-        'description': obj.description
+        'object': obj
     }
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
